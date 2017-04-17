126be624da8caf1f1b43e9f4bac487935d61ed84
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2017-04-17 22:12:29 +0200

    BORN

---
+++ src/main/java/io/unicorn/MvpDiscoveryPatternApplication.java
@@ -0,0 +1,12 @@
+package io.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpDiscoveryPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpDiscoveryPatternApplication.class, args);
+	}
+}
